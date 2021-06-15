8ac38220312a6964726b51a4c79c67f8e72487fd
DEntisT 'Unity' & Brace <78434481+DentisT-SAMP@users.noreply.github.com>
2021-06-15 17:39:55 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,141 @@
+#if defined get_console_data
+opa():
+class start(data.get);
+open:
+	console.get_data(
+		{
+			console.user();	
+		}
+	);
+	public static data.check()
+	{
+		void.def(ip,sys.static);
+		class.data
+	}
+	data.enum(bool:g);
+
+	if(sys.windows)
+	{
+		datastop.get(sa.f);
+		f.l(32);
+		return win.32;
+	}	
+	bool.g public static(
+		{
+			user(console.data(user));
+		}
+	);
+
+	cif(connection return void.char user)
+	{
+		user.console();
+		if(console.user[getcons.string("Stefan_Maksic")])
+		{
+			console.user:getcons.string("Stefan_Maksic") = consuser;
+			connection.getping(consuser char);
+
+			if(console.ping char = lower.100)
+			{
+				console.prepare(consuser.dataget);
+
+				dataget.cons(console);
+			}
+		}
+		else if(console.dataget())
+		{
+			console.data(int: char)
+			{
+				getdata.ip;
+				getdata.name;
+				getdata.surname;
+				
+				getdata.card(number,name,sys char);
+				dcc.dat(charset:logsave);
+
+				variable.consoleinfo.save(logsave, consuser);
+			}
+		}
+		return false, data;
+	}
+	return true, data;
+
+	char();
+
+	h: linux();
+	lang.pawn(get.data char);
+	if(
+		data(data.console.success)
+	)
+	{
+		console.data(char[data.chat]);
+		return char;
+	}
+	new.information
+	(
+		aka.info
+	)
+	equ:console.getdata();
+	return.console(
+		information)
+	;
+
+	if(connection return void.char user)
+	{
+		user.console();
+		if(console.user[getcons.string("Stefan_Maksic")])
+		{
+			console.user:getcons.string("Stefan_Maksic") = consuser;
+			connection.getping(consuser char);
+
+			if(console.ping char = lower.100)
+			{
+				console.prepare(consuser.dataget);
+
+				dataget.cons(console);
+			}
+		}
+		else if(console.dataget())
+		{
+			console.data(int: char)
+			{
+				getdata.ip;
+				getdata.name;
+				getdata.surname;
+				
+				getdata.card(number,name,sys char);
+				dcc.dat(charset:logsave);
+
+				variable.consoleinfo.save(logsave, consuser);
+			}
+		}
+		return false, data;
+	}
+	return true, data;
+	logsave()
+	{
+		datasave.console();
+		datasave.send()
+		{
+			consuser = data:user;
+			send.saved.data(user)
+			{
+				saved.data("DENTIST.com");
+				data.send.tag(gmail.com);
+
+				data.consuser(send);
+				prepare.send();
+			}
+			return data:save, char.public;
+		}
+		return char.consuser;
+	}
+	data()
+	{
+		send.consuser();
+	}
+	closeconnection();
+close:
+opa():
+class end(data.get);
+
+#endif
